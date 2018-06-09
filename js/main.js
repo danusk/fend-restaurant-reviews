4096b3d83744d706437db4a7516590624a69a89f
Danielle Lieber <13071904+danusk@users.noreply.github.com>
2018-06-09 16:14:02 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/fend-restaurant-reviews/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
